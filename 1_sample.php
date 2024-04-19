21a6ee166cae6b52b737e0435a874e1036273607
Hadji Tejuco <hadji.tejuco@gmail.com>
2024-04-19 11:28:52 +0800

    first commit

---
+++ 1_sample.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>My First PHP script</title>
+    </head>
+    <body>
+        <h1>Using PHP script</h1>
+        <p>This will display hello world with current date</p>
+        
+        <?php
+            //comments
+            echo "Hello World";
+            //display current date
+            $today = date('Y-m-d');
+            //display the date
+            echo "<p><b>Today is $today </b></p>";
+            
+        ?>
+    </body>
+</html>
