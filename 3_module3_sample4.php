3b3653ab18f4e797fed1f19213c4e8f176fb607e
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2024-04-29 11:31:59 +0800

    Create 3_module3_sample4.php

---
+++ 3_module3_sample4.php
@@ -0,0 +1,23 @@
+<?php
+//no parameters
+// function sayHello() {
+//     echo "Hello World";
+
+// }
+// sayHello(); //calls the function 
+
+
+// //functions with parameters
+// function greet($name) {
+//     echo "Hello , $name! ";
+// }
+// greet("Hadji");
+
+//function that returns a value
+function add($a, $b) {
+    return $a + $b;
+}
+
+$result = add(5,5);
+echo $result;
+?>
