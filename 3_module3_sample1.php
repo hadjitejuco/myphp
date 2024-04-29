7f489315fa5d06357c18647ecfcacb871c53b199
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2024-04-29 10:34:42 +0800

    Create 3_module3_sample1.php

---
+++ 3_module3_sample1.php
@@ -0,0 +1,29 @@
+<?php
+    $array = array(1,2,3,4,5);
+    print_r($array);
+    echo "<br>";
+    var_dump($array);
+
+    echo "<br>";
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+
+    echo "<br>";
+    echo "<pre>";
+    var_dump($array);
+    echo "</pre>";
+
+    $array = array('apple','banana','cherry');
+    print_r($array);
+    echo "<br>";
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+    echo "<br>";
+       echo "<br>";
+    echo "<pre>";
+    var_dump($array);
+    echo "</pre>";
+
+?>
