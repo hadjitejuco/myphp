af53c2bfbcd7f6d9b54001901666156cb7788348
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2024-05-10 12:10:26 +0800

    Add files via upload

---
+++ module4/strf.php
@@ -0,0 +1,47 @@
+<?php
+echo "strlen()";
+echo "<br>";
+echo strlen("Hello");
+echo "<br>";
+echo strlen("Hello World");
+echo "<hr>";
+echo "strpos()";
+echo "<br>";
+echo strpos("Hello World", "World");
+echo "<br>";
+echo strpos("Hello World", "xyz");
+echo "<br>";
+echo strpos("Hello World", "H");
+echo "<br>";
+echo "<hr>";
+echo "strrev()";
+echo "<br>";
+echo strrev("World");
+echo "<br>";
+echo "<hr>";
+echo "strtolower()";
+echo "<br>";
+echo strtolower("WORlD");
+echo "<br>";
+echo "<hr>";
+echo "strtoupper()";
+echo "<br>";
+echo strtoupper("worlD");
+echo "<br>";
+echo "<hr>";
+echo "substr()";
+echo "<br>";
+echo substr("Hello World",6,4);
+echo "<br>";
+echo "<br>";
+echo substr("Hello",0,4);
+
+
+
+
+
+
+
+
+
+?>
