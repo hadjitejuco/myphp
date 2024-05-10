1e88400a334a81372e6d0d0e5979da3af28153f4
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2024-05-10 11:23:36 +0800

    Add files via upload

---
+++ module4/mathf.php
@@ -0,0 +1,32 @@
+<?php
+	//rand()
+	echo "rand()";
+	echo "<br/>";
+	echo rand(0,100);
+	echo "<br/>";
+	echo rand(50,100);
+	echo "<hr>";
+	echo "ceil()";
+	echo "<br/>";
+	echo ceil(4.3);
+	echo "<br/>";
+	echo ceil(-3.6);
+	echo "<hr>";
+	echo "floor()";
+	echo "<br/>";
+	echo floor(4.3);
+	echo "<br/>";
+	echo floor(-3.6);
+	echo "<hr>";
+	echo "min()";
+	echo "<br/>";
+	echo min([4,3,7,9,5,1]);
+	echo "<br/>";
+	echo min(5,2);
+	echo "<hr>";
+	echo "max()";
+	echo "<br/>";
+	echo max([4,3,7,9,5,1]);
+	echo "<br/>";
+	echo max(5,2);
+?>
